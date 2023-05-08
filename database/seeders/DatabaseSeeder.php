6cd2eb3e457c7c492cb232ca8d3d2bac2519797a
dedektegarr <dedektegarr@gmail.com>
2023-05-08 11:44:33 +0700

    menghapus migrasi kecamatan dan kelurahan serta model dan factory nya

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,20 +14,6 @@ class DatabaseSeeder extends Seeder
     {
         \App\Models\User::factory(1)->create();
 
-        \App\Models\Kecamatan::factory()->createMany(
-            [
-                ['nama_kecamatan' => 'Sungai Serut'],
-                ['nama_kecamatan' => 'Gading Cempaka'],
-                ['nama_kecamatan' => 'Selebar'],
-                ['nama_kecamatan' => 'Kampung Melayu'],
-                ['nama_kecamatan' => 'Muara Bangka Hulu'],
-                ['nama_kecamatan' => 'Ratu Agung'],
-                ['nama_kecamatan' => 'Ratu Samban'],
-                ['nama_kecamatan' => 'Singaran Pati'],
-                ['nama_kecamatan' => 'Teluk Segara'],
-            ]
-        );
-
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
