d9e5542864bdc09e54e75ba207e93c91d903b76f
dedektegarr <dedektegarr@gmail.com>
2023-05-09 23:29:42 +0700

    fix/memperbaiki form select2

--- resources/views/lokasi/index.blade.php
+++ resources/views/lokasi/index.blade.php
@@ -12,24 +12,20 @@
                     <div class="card-body">
                         <div class="form-group">
                             <label for="kota_kabupaten">Kota / Kabupaten</label>
-                            <select class="form-control" name="kota_kabupaten" id="kota_kabupaten">
+                            <select class="form-control select2" name="kota_kabupaten" id="kota_kabupaten">
+                                <option value="">Pilih Kota / Kabupaten</option>
                             </select>
                         </div>
                         <div class="form-group">
                             <label for="kecamatan">Kecamatan</label>
-                            <select class="form-control" name="kecamatan" id="kecamatan">
+                            <select class="form-control select2" name="kecamatan" id="kecamatan" disabled>
+                                <option value="">Pilih Kecamatan</option>
                             </select>
                         </div>
                         <div class="form-group">
                             <label for="kelurahan_desa">Kelurahan / Desa</label>
-                            <select class="form-control" name="kelurahan_desa" id="kelurahan_desa">
+                            <select class="form-control select2" name="kelurahan_desa" id="kelurahan_desa" disabled>
                                 <option value="">Pilih Kelurahan / Desa</option>
-                            </select>
-                        </div>
-                        <div class="form-group">
-                            <label for="nama_jalan">Nama Jalan</label>
-                            <select class="form-control select2" name="nama_jalan" id="nama_jalan">
-                                <option value="">Pilih Jalan</option>
                             </select>
                         </div>
                         <div class="form-group">
@@ -50,28 +46,17 @@
 @section('script')
     {{-- SELECT2 PLUGIN --}}
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+    <script src='https://api.mapbox.com/mapbox-gl-js/v2.14.1/mapbox-gl.js'></script>
+
     <script>
+        // Inisialisasi Select2
         $(document).ready(function() {
+            const data = ['test', 'dasfd', 'asdf'];
             $('.select2').select2();
         });
 
-        // Base API
-        const API = "https://dev.farizdotid.com/api/daerahindonesia";
-
-        const getAPI = async (url) => {
-            const response = await fetch(url);
-            const data = await response.json();
-            return data;
-        }
-
-        // FORM ELEMENT
-        const formKota = document.querySelector('#kota_kabupaten');
-        const formKecamatan = document.querySelector('#kecamatan');
-        const formKelurahan = document.querySelector('#kelurahan_desa');
-        const formJalan = document.querySelector('#nama_jalan');
-
+        // Create Option Element Function
         const newOptionElement = (parent, data) => {
-            parent.innerHTML = '';
             const options = Array.isArray(data) ? data : [data];
 
             for (let data of options) {
@@ -82,24 +67,57 @@
             }
         }
 
-        window.addEventListener('DOMContentLoaded', async () => {
-            const kota = await getAPI(`${API}/kota/1771`);
-            newOptionElement(formKota, kota);
+        // enamble element select
+        const enableSelect = (element, isDisable) => {
+            if (isDisable) {
+                element.removeAttr('disabled');
+            } else {
+                element.attr('disabled', true);
+            }
+        }
 
-            const kecamatan = await getAPI(`${API}/kecamatan?id_kota=${formKota.value}`);
-            newOptionElement(formKecamatan, kecamatan.kecamatan);
+        // API Wilayah Indonesia
+        // Base API
+        const API = "https://dev.farizdotid.com/api/daerahindonesia";
+        // Mapbox TOKEN
+        mapboxgl.accessToken =
+            'pk.eyJ1IjoiZGVkZWt0ZWdhciIsImEiOiJjbGVjdnJkY3cwMHl5M3BxanYwc2dueWNsIn0.nBiB8NlOPqhCxpnqgK4glA';
 
-            const kelurahan = await getAPI(`${API}/kelurahan?id_kecamatan=${formKecamatan.value}`);
-            newOptionElement(formKelurahan, kelurahan.kelurahan);
+        $.get(`${API}/kota/1771`, function(response) {
+            newOptionElement($('#kota_kabupaten'), response);
         });
 
-        formKecamatan.addEventListener('change', (e) => {
-            formKelurahan.setAttribute('disabled', 'true');
-            if (e.target.value) {
-                formKelurahan.removeAttribute('disabled');
-                getAPI(`${API}/kelurahan?id_kecamatan=${formKecamatan.value}`)
-                    .then(data => newOptionElement(formKelurahan, data.kelurahan));
+        $('#kota_kabupaten').on('select2:select', function(e) {
+            const id = e.params.data.id;
+            enableSelect($('#kecamatan'), id);
+            if (id) {
+                $.get(`${API}/kecamatan?id_kota=${id}`, function(response) {
+                    newOptionElement($('#kecamatan'), response.kecamatan);
+                });
             }
         });
+
+        $('#kecamatan').on('select2:select', function(e) {
+            const id = e.params.data.id;
+            enableSelect($('#kelurahan_desa'), id);
+            if (id) {
+                $.get(`${API}/kelurahan?id_kecamatan=${id}`, function(response) {
+                    newOptionElement($('#kelurahan_desa'), response.kelurahan);
+                });
+            }
+        });
+
+        $('#kelurahan_desa').on('select2:select', function(e) {
+            const id = e.params.data.id;
+            enableSelect($('#nama_jalan'), id);
+        });
+
+        $('#nama_jalan').on('select2:selecting select2:typing', function(e) {
+            const inputValue = e.params.args.data.text;
+            console.log('Data yang diketik:', inputValue);
+        });
+        // $('#nama_jalan').on('select2:select', function(e) {
+        //     console.log(e.params);
+        // });
     </script>
 @endsection
