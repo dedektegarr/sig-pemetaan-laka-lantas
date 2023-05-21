c68d10e037601d33fb8cf269a480ace0fccf021f
dedektegarr <dedektegarr@gmail.com>
2023-05-21 19:02:42 +0700

    mengubah icon menu dashboard

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -40,7 +40,7 @@
                  <li class="nav-item">
                      <a href="{{ route('dashboard.index') }}"
                          class="nav-link {{ Route::is('dashboard.index') ? 'active' : '' }}">
-                         <i class="nav-icon fas fa-th"></i>
+                         <i class="nav-icon fas fa-home"></i>
                          <p>
                              Dashboard
                          </p>
