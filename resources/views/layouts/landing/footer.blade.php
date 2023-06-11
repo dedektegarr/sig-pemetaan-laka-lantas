2190ce93dd9df49fb8ed34f89512f5a8b04df758
dedektegarr <dedektegarr@gmail.com>
2023-06-11 09:53:11 +0700

    pre merge

---
+++ resources/views/layouts/landing/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+
+    <div class="float-right d-none d-sm-inline">
+        Sistem Informasi Geografis Pemetaan Lokasi Kejadian Kecelakaan Lalu Lintas Kota Bengkulu
+    </div>
+
+    <strong>Copyright {{ date('Y') }} <a href="{{ env('APP_URL') }}">SIGKKL</a>.</strong> All rights reserved.
+</footer>
